61ed11006dbe10e9bef8edaad1c26ab4057c59b2
Avadhut <kodtodya@gmail.com>
2024-04-21 12:54:13 +0530

    added do while logic

--- src/com/kodtodya/practice/pattern/Pattern1.java
+++ src/com/kodtodya/practice/pattern/Pattern1.java
@@ -23,11 +23,23 @@ public class Pattern1 {
 //            System.out.println();
 //            lineNo++;
 //        }
+
+        System.out.println("WHILE loop demonstration:");
+        // while loop demonstration
         int lineNo = -1;
         while(++lineNo < PATTERN_SIZE) {
             int index = -1;
             while(++index <= lineNo) {
                 //System.out.print("*[line:"+ lineNo +",index:"+index+"]\t");
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+        // for loop demonstration
+        System.out.println("\n\nFOR loop demonstration:");
+        for(int line = 0; line < PATTERN_SIZE;line++) {
+            for(int index = 0; index <= line;index++) {
                 System.out.print("*\t");
             }
             System.out.println();
--- src/com/kodtodya/practice/pattern/Pattern2.java
+++ src/com/kodtodya/practice/pattern/Pattern2.java
@@ -8,10 +8,11 @@ public class Pattern2 {
     // * * *      -> line#2
     // * *        -> line#3
     // *          -> line#4
-    private static final int PATTERN_SIZE = 10;
+    private static final int PATTERN_SIZE = 7;
 
     public static void main(String[] args) {
 
+        System.out.println("WHILE loop demonstration");
         int lineNo = 0;
         while (lineNo < PATTERN_SIZE) {
             int index = PATTERN_SIZE - lineNo;
@@ -28,6 +29,20 @@ public class Pattern2 {
 
             System.out.println();
             lineNo++;
+        }
+
+        System.out.println("\n\nFOR loop demonstration");
+
+        for (int line = 0; line < PATTERN_SIZE; line++) {
+            for (int index = PATTERN_SIZE - line; index > 0; index--) {
+                System.out.print("*\t");
+            }
+
+//            for (int index = 0; index < PATTERN_SIZE - lineNo; index++) {
+//                System.out.print("*\t");
+//            }
+
+            System.out.println();
         }
     }
 }
